6e61da35052914fff75a9a4a5a0db3e8e1b2fe11
MIGUEL A González <magonzalez@teleconline.com>
2025-02-23 19:01:43 +0100

    Create qr.py
    creado

---
+++ qr.py
@@ -0,0 +1,36 @@
+import streamlit as st 
+st.logo("logo-aedas.svg")
+
+
+
+
+#st.title('Bienvenido a AEDAS Homes')
+#st.header('Gestión de incidencias postventa')
+
+#st.divider()
+#st.subheader('Promoción: KANE')
+st.subheader('Vivienda en KANE, Bloque 1, Escalera 1, Planta 2, Letra B')
+
+#st.divider()
+st.info('Incidencia 325416_001, oficio PINTURA', icon="ℹ️")
+
+st.text_area('Por favor, describa el trabajo ralizado')
+#img_file_buffer = st.camera_input("Adjuntar fotografía",label_visibility="collapsed")
+st.button("Adjuntar fotografía")
+
+st.text_input('Nombre y apellidos')
+st.text_input('DNI del firmante')
+#st.divider()
+
+#options = ["Conforme", "No conforme"]
+#selection = st.segmented_control(
+#    "Conformidad con la ejecución de la tarea", options, selection_mode="single", default="Conforme")
+
+on = st.toggle("¿Conforme con la ejecución?",value=1)
+
+if on:
+    st.write("Conforme con la ejecución de la tarea")
+else:
+    st.write("No conforme")
+st.text_area('Firma del propietario-contacto')
+#st.divider()
